9fc66a7d36713de2dfb6285933d2273318fd980b
Education-cite <tusarjobs@gmail.com>
2023-04-13 23:03:46 +0600

    a

---
+++ src/ExtraPractice/fibonacci.java
@@ -0,0 +1,47 @@
+package ExtraPractice;
+
+import java.util.Scanner;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+			
+		
+		
+		int i;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please Enter Your Number");
+		int n = s.nextInt();
+		int fact = 1;
+		for(i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		
+		System.out.println(n+" factorial number is :"+fact);
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	}
+
