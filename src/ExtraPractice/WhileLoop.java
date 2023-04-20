cf34410ca853b74f8bbc01f41484d03289744170
Education-cite <tusarjobs@gmail.com>
2023-04-20 15:43:58 +0600

    h

---
+++ src/ExtraPractice/WhileLoop.java
@@ -0,0 +1,14 @@
+package ExtraPractice;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i ;
+		int x=2;
+		
+		for(i=1;i<=10;i++){
+			System.out.println(x+ " * "+ i + " = " + x*i);
+			
+		}
+	}
+}
