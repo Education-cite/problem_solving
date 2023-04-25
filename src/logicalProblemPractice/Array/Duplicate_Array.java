9584201ec55a2cfac6482b09422194ad21fc9e04
Education-cite <tusarjobs@gmail.com>
2023-04-25 17:19:15 +0600

    d

---
+++ src/logicalProblemPractice/Array/Duplicate_Array.java
@@ -0,0 +1,19 @@
+package logicalProblemPractice.Array;
+
+public class Duplicate_Array {
+
+	public static void main(String[] args) {
+		int []a = new int[] {1,2,3,4,4,2,7,8,7};
+        System.out.println("Duplicate elements in given array: ");  
+
+		for(int i=0;i<a.length;i++) {
+			for(int j = i+1;j<a.length;j++) {
+
+			if(a[j]==a[i]) {
+				System.out.print(a[j]+" ");
+
+			}
+			}
+		}
+	}
+}
---
+++ src/logicalProblemPractice/Array/Element_Of_Array.java
@@ -0,0 +1,18 @@
+package logicalProblemPractice.Array;
+
+public class Element_Of_Array {
+public static void main(String[] args) {
+	
+	
+	int a[] = new int[] {3,5,7,2,9};
+	
+	for(int i=0; i<a.length; i++) {
+		System.out.print(a[i]+" ");
+		
+	}
+	
+	
+	
+	
+}
+}
